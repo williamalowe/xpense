ea566aec0cbdde76161420d7e6085051eb55c6e0
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-12 18:30:25 +1000

    wip: add results component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Container from "@/components/container"
+import Results from "@/components/results";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <Container>
           <div className="flex-[3]"></div>
-          <div className="flex-[2] rounded-bl-[4rem] bg-sky-800"></div>
+          <Results />
       </Container>
     </main>
   );
