817773b8043c67c451702e678c0d7e02383b07d2
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-14 15:53:53 +1000

    func: add addExpense functionality

--- src/components/new-expense-form.tsx
+++ src/components/new-expense-form.tsx
@@ -1,39 +1,96 @@
 "use client";
+const { v4: uuidv4 } = require("uuid");
 import { useExpense } from "@/providers/expense-context";
 import { useState } from "react";
 import { BsDash, BsPlus } from "react-icons/bs";
 
-export default function NewExpenseForm({ occurance }: {
-  occurance: string
-}) {
-  const {addExpense} = useExpense();
+export default function NewExpenseForm({ occurance }: { occurance: string }) {
+  const { addExpense } = useExpense();
   const [showForm, setShowForm] = useState(false);
+  const [newExpenseName, setNewExpenseName] = useState<string>("");
+  const [newExpenseValue, setNewExpenseValue] = useState<number>(0.0);
+  const [showError, setShowError] = useState(false);
+
+  const resetFields = () => {
+    setShowForm(false);
+    setNewExpenseName("");
+    setNewExpenseValue(0);
+    setShowError(false);
+  };
+
+  const handleSubmit = () => {
+    const newExpense = {
+      id: uuidv4(),
+      expense: newExpenseName,
+      value: Math.round(newExpenseValue * 100) / 100,
+      occurance: occurance,
+    };
+
+    if (newExpense.expense === "" || newExpense.value < 1) {
+      setShowError(true);
+    } else {
+      addExpense(newExpense);
+      resetFields();
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
-      <button 
-      onClick={() => setShowForm(!showForm)}
-      className="mb-4 w-4 h-4 flex items-center justify-center bg-emerald-400 rounded text-black" 
+      <button
+        onClick={() => setShowForm(!showForm)}
+        className="mb-4 w-4 h-4 flex items-center justify-center bg-emerald-400 rounded text-black"
       >
-        {
-          showForm ? <BsDash /> : <BsPlus />
-        }
+        {showForm ? <BsDash /> : <BsPlus />}
       </button>
-      {
-        showForm &&
-        <form onSubmit={(e) => e.preventDefault} className="w-full bg-white rounded text-black p-2 flex flex-col gap-y-2">
+      {showForm && (
+        <form
+          onSubmit={(e) => e.preventDefault}
+          className="w-full bg-white rounded text-black p-2 flex flex-col gap-y-2"
+        >
           <div className="flex">
-            <label className="bg-emerald-400 px-2 rounded-l-md border border-emerald-400">Expense</label>
-            <input type="text" className="border border-emerald-400 px-2 rounded-r-md w-[100%]" />
+            <label className="bg-emerald-400 px-2 rounded-l-md border border-emerald-400">
+              Expense
+            </label>
+            <input
+              type="text"
+              className="border border-emerald-400 px-2 rounded-r-md w-[100%]"
+              value={newExpenseName}
+              onChange={(e) => setNewExpenseName(e.target.value)}
+              placeholder={
+                occurance === "daily"
+                  ? "Morning Coffee"
+                  : occurance === "weekly"
+                  ? "Grocery Shopping"
+                  : "Rent"
+              }
+            />
           </div>
           <div className="flex">
-          <div className="flex">
-            <label className="bg-emerald-400 px-2 rounded-l-md border border-emerald-400">$</label>
-            <input type="number" className="border border-emerald-400 px-2 rounded-r-md w-[25%]" />
+            <div className="flex">
+              <label className="bg-emerald-400 px-2 rounded-l-md border border-emerald-400">
+                $
+              </label>
+              <input
+                type="number"
+                className="border border-emerald-400 px-2 rounded-r-md w-[25%]"
+                value={newExpenseValue}
+                min={0}
+                onChange={(e) => setNewExpenseValue(e.target.valueAsNumber)}
+              />
+            </div>
+            <button
+              className="ml-auto px-2 bg-emerald-400 rounded"
+              onClick={handleSubmit}
+            >
+              Add
+            </button>
           </div>
-            <button className="ml-auto px-2 bg-emerald-400 rounded">Add</button>
-          </div>
-        </form>
-      }
+          {/* {
+            showError && 
+            <p>Oops! Something went wrong. Please make sure your inputs are valid.</p>
+          } */}
+        </form> 
+      )}
     </div>
   );
 }
