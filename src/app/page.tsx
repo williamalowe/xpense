061803145c56d5718959a66a4ff8db196ccc2590
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-14 16:32:52 +1000

    feat: add outpuits component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import ExpenseList from "@/components/expense-list";
+import Outputs from "@/components/outputs";
 
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center p-8">
       <div className="h-full w-full bg-zinc-800 rounded-[1rem] flex flex-col overflow-hidden">
-        <div className="flex-1 rounded-br-[2rem] bg-white"></div>
+        <div className="flex-1 rounded-br-[2rem] bg-white">
+          <Outputs />
+        </div>
         <div className="h-[65vh] bg-zinc-800 text-white p-4 overflow-scroll no-scrollbar">
           <ExpenseList 
             title="daily"
