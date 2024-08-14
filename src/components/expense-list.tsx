1fa7948654bee62f602146c607b9110dc6e5d555
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-14 14:48:11 +1000

    feat: add expense-list component

--- src/components/expense-list.tsx
+++ src/components/expense-list.tsx
@@ -13,7 +13,26 @@ export default function ExpenseList({ title, expenses }: {
   return (
     <div>
       <h3 className="font-bold text-center text-lg"><span className="text-emerald-400">{title}</span> Expenses</h3>
-      <ul></ul>
+      <ul>
+        <li className="flex items-center gap-x-4">
+          <h5>Coffee</h5>
+          <p className="ml-auto text-sm">
+            $5.00
+            {
+              title === "Daily" ? " p/day" : title === "Weekly" ? " p/week" : " p/month"
+            }
+          </p>
+        </li>
+        <li className="flex items-center gap-x-4">
+          <h5>Coffee</h5>
+          <p className="ml-auto text-sm">
+            $5.00
+            {
+              title === "Daily" ? " p/day" : title === "Weekly" ? " p/week" : " p/month"
+            }
+          </p>
+        </li>
+      </ul>
     </div>
   )
 }
