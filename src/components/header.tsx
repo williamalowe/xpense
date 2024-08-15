e1de1d576394930c418693f132620de5e49939ce
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-15 12:42:53 +1000

    styles: adjust header component positioning

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <h3 className="absolute text-white italic top-0">
+    <h3 className="absolute text-white italic top-1">
       XPense - Expense Tracking
     </h3>
   )
