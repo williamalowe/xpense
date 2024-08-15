6b6e41ceed44891e31952b331fe4e7d923e98aa9
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-15 12:37:30 +1000

    feat: add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ExpenseList from "@/components/expense-list";
+import Header from "@/components/header";
 import Outputs from "@/components/outputs";
 
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center p-8">
+    <main className="flex flex-col h-screen items-center justify-center p-8">
+      <Header />
       <div className="h-full w-full bg-zinc-800 rounded-[1rem] flex flex-col overflow-hidden">
         <div className="flex-1 rounded-br-[2rem] bg-white">
           <Outputs />
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <h3 className="absolute text-white italic top-0">
+      XPense - Expense Tracking
+    </h3>
+  )
+}
