a3a1b70e3bef4ab89d5372f31daa11b6cbbc4069
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-15 12:34:50 +1000

    format: prettier

--- src/components/outputs.tsx
+++ src/components/outputs.tsx
@@ -23,10 +23,27 @@ export default function Outputs() {
   const total = evaluateTotal();
   return (
     <div className="p-4 flex flex-col">
-      <h3 className="font-bold text-2xl hover:italic hover:translate-x-4 transition"><span className="text-4xl text-emerald-600">${total}</span> Yearly</h3>
-      <h5 className="font-bold text-xl opacity-60 hover:opacity-100 hover:italic hover:translate-x-4 transition"><span className="text-2xl text-emerald-600">${Math.round((total / 12) * 100) / 100}</span> Monthly</h5>
-      <h5 className="font-bold text-xl opacity-60 hover:opacity-100 hover:italic hover:translate-x-4 transition"><span className="text-2xl text-emerald-600">${Math.round((total / 52) * 100) / 100}</span> Weekly</h5>
-      <h5 className="font-bold text-xl opacity-60 hover:opacity-100 hover:italic hover:translate-x-4 transition"><span className="text-2xl text-emerald-600">${Math.round((total / 365) * 100) / 100}</span> Daily</h5>
+      <h3 className="font-bold text-2xl hover:italic hover:translate-x-4 transition">
+        <span className="text-4xl text-emerald-600">${total}</span> Yearly
+      </h3>
+      <h5 className="font-bold text-xl opacity-60 hover:opacity-100 hover:italic hover:translate-x-4 transition">
+        <span className="text-2xl text-emerald-600">
+          ${Math.round((total / 12) * 100) / 100}
+        </span>{" "}
+        Monthly
+      </h5>
+      <h5 className="font-bold text-xl opacity-60 hover:opacity-100 hover:italic hover:translate-x-4 transition">
+        <span className="text-2xl text-emerald-600">
+          ${Math.round((total / 52) * 100) / 100}
+        </span>{" "}
+        Weekly
+      </h5>
+      <h5 className="font-bold text-xl opacity-60 hover:opacity-100 hover:italic hover:translate-x-4 transition">
+        <span className="text-2xl text-emerald-600">
+          ${Math.round((total / 365) * 100) / 100}
+        </span>{" "}
+        Daily
+      </h5>
     </div>
   );
 }
