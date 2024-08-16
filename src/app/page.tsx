3502e81f172063ed1dc8ed0c4186185c2ea61ae5
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-16 12:23:21 +1000

    styles: add sm resolution styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <main className="flex flex-col h-screen items-center justify-center p-8">
       <Header />
-      <div className="h-full w-full bg-zinc-800 rounded-[1rem] flex flex-col overflow-hidden">
-        <div className="flex-1 rounded-br-[2rem] bg-white">
+      <div className="h-full w-full bg-zinc-800 rounded-[1rem] flex flex-col overflow-hidden sm:flex-row">
+        <div className="flex-1 rounded-br-[2rem] bg-white sm:flex-none">
           <Outputs />
         </div>
-        <div className="h-[65vh] bg-zinc-800 text-white p-4 overflow-scroll no-scrollbar">
+        <div className="h-[65vh] bg-zinc-800 text-white p-4 overflow-scroll no-scrollbar sm:h-full sm:flex-1">
           <ExpenseList 
             title="daily"
           />
